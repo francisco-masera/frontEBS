14aa3111095e3a81572793e2c620612df260a743
Georgina <georgi888111@gmail.com>
2020-05-29 14:11:05 -0300

    avances con el login

--- frontelbuensabor/src/App.vue
+++ frontelbuensabor/src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav"><top-nav-bar></top-nav-bar></div>
-    <router-view />
+   
+  <router-view />
   </div>
 </template>
 
 <script>
-import NavBarTop from "@/components/TopBar.vue";
+
 export default {
-  components: {
-    "top-nav-bar": NavBarTop
-  }
+
   
 };
 </script>
---
+++ frontelbuensabor/src/views/Ingreso.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+      
+   <b-modal ref="miModal" hide-footer hide-header>
+   <div>    
+   <b-label>Ingresar</b-label>    
+   </div>
+   <div>
+   <b-button id="loginGoogle">Ingresar con google</b-button>
+   </div>
+   <div>O ingresá con tu email</div>
+      <form>
+           <div>
+           <b-form-input
+            id="email-input"
+            v-model="email"
+            :state="emailState"
+            required
+            type="email"
+            placeholder="email"
+          ></b-form-input>
+           </div>
+            <div>
+           <b-form-input
+            id="contraseña-input"
+            v-model="contraseña"
+            :state="contraseñaState"
+            required
+            type="password"
+            placeholder="contraseña"
+          ></b-form-input>
+          </div>
+        <div>Olvidé mi contraseña</div>
+       <b-button class="mr-1" >Ingresar</b-button>
+      </form>
+      <div>¿No tenes cuenta? Registrate</div>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  name: "Ingreso",
+  components: {},
+  mounted() {
+    this.openModal();
+  },
+   data() {
+    return {
+     name: '',
+     nameState: null  
+    
+   }
+   },
+  methods: {
+    openModal() {
+     
+      this.$refs["miModal"].show();
+    }
+  }
+  
+};
+</script>
+<style>
+.styleModal{
+   color: blue; 
+}
+</style>
