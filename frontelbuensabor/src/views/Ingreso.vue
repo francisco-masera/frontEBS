3610ec6d0f86129e0a69bc7a5b59aa3a22f9db8c
Georgina <georgi888111@gmail.com>
2020-05-30 20:22:20 -0300

    login casi listo

--- frontelbuensabor/src/views/Ingreso.vue
+++ frontelbuensabor/src/views/Ingreso.vue
@@ -1,67 +1,135 @@
 <template>
-  <div>
-      
-   <b-modal ref="miModal" hide-footer hide-header size="sm" id="modal-center">
-   <div>    
-   <b-label>Ingresar</b-label>    
-   </div>
-   <div>
-   <b-button id="loginGoogle">Ingresar con google</b-button>
-   </div>
-   <div>O ingresá con tu email</div>
-      <form>
-           <div>
+<b-container fluid class="ingreso">
+ <b-row> 
+ <b-col xs="6" sm="6" md="3" lg="3" xl="3"></b-col> 
+ <b-col class="margenLogo" xs="12" sm="12" md="6" lg="6" xl="6"><b-img src="@/assets/images/sistema/logo-01.png" style="width:35%" ></b-img></b-col> 
+ <b-col xs="6" sm="6" md="3" lg="3" xl="3"></b-col> 
+</b-row>
+<b-row>
+  <b-col  xs="6" sm="6" md="3" lg="3" xl="3"></b-col> 
+  <b-col  xs="12" sm="12" md="6" lg="6" xl="6">
+     <div class="ingresoForm"><form>
+        <div class="tituloIngreso"><b-label>Ingresar</b-label></div>  
+          
+             
            <b-form-input
             id="email-input"
             v-model="email"
             :state="emailState"
             required
             type="email"
-            placeholder="email"
+            placeholder="Usuario" class="emailForm"
           ></b-form-input>
-           </div>
-            <div>
+           
+           
            <b-form-input
             id="contraseña-input"
             v-model="contraseña"
             :state="contraseñaState"
             required
             type="password"
-            placeholder="contraseña"
+            placeholder="Contraseña" class="contraseñaForm"
           ></b-form-input>
-          </div>
-        <div>Olvidé mi contraseña</div>
-       <b-button class="mr-1" >Ingresar</b-button>
+          
+         
+        <div class="olvido">Olvidé mi contraseña</div>
+       <b-button pill class="botonIngreso" size="md">¡Ingresar!</b-button>
       </form>
-      <div>¿No tenes cuenta? Registrate</div>
-    </b-modal>
-  </div>
+     </div>
+    </b-col> 
+  <b-col  xs="6" sm="6" md="3" lg="3" xl="3"></b-col> 
+</b-row> 
+<b-row>
+<b-col class="abajo"  xs="12" sm="12" md="12" lg="12" xl="12"></b-col>
+</b-row>
+</b-container>
+
 </template>
-<script>
-export default {
-  name: "Ingreso",
-  components: {},
-  mounted() {
-    this.openModal();
-  },
-   data() {
-    return {
-     name: '',
-     nameState: null  
-    
-   }
-   },
-  methods: {
-    openModal() {
-     
-      this.$refs["miModal"].show();
-    }
-  }
-  
-};
-</script>
 <style>
-.styleModal{
-   color: blue; 
+
+@import url('https://fonts.googleapis.com/css2?family=Baloo+Bhaina+2:wght@400;500;600;700;800&display=swap');
+.prueba{
+  border:1px solid gray;
+}
+
+.emailForm{
+  border-right:0px;
+  border-left:0px;
+  border-top:0px;
+  border-bottom:1;
+  background-color:transparent;
+  width: 300px;
+  font-size: 11pt;
+ margin-left: 10%;
+}
+
+.contraseñaForm{
+  border-right:0px;
+  border-left:0px;
+  border-top:0px;
+  border-bottom:1;
+  background-color:transparent;
+  width: 300px;
+  font-size: 11pt;
+  margin-left: 10%;
+}
+.abajo{
+
+background-image: url("../assets/images/sistema/reticula.png");
+background-repeat: repeat-x;
+width: 100%;
+background-color:  #F2E6D1;
+min-height: 60px;
+background-size: contain;
+bottom: 0;
+margin-top: 0px
+;
+
+ 
+}
+   
+.olvido{
+  text-align: right;
+  margin-top: 5%;
+  font-size: 10pt;
+  margin-right: 5%;
+}
+.tituloIngreso{
+  margin-top: 7%;
+  margin-bottom: 3%;
+  font-weight: bold;
+  font-size: 15pt;
+}
+.margenLogo{
+margin-top: 3%;
+}
+.ingreso{
+  background-color: #F2E6D1;
+  font-family: 'Baloo Bhaina 2';
+}
+
+.botonIngreso{
+  background-color: #E7511E;
+  width: 105px;
+  height: 30px;
+  margin-top: 5%;
+  
+
+
+}
+.ingresoForm{
+
+
+  margin-top: 7%;
+  margin-bottom: 20%;
+  margin-left: 19%;
+  height: 245px;
+  width: 370px;
+  background-color: white;
+  border:1px solid gray;
+  
+ 
+ 
+  
 }
 </style>
