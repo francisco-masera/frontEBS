eeadf8cac52ea9ef1233e6cd849b4f114879aba7
Georgina <georgi888111@gmail.com>
2020-06-01 14:44:53 -0300

    catalogo manufacturados

--- frontelbuensabor/src/router/index.js
+++ frontelbuensabor/src/router/index.js
@@ -4,7 +4,7 @@ import Home from '../views/Home.vue'
 import Ingreso from '../views/Ingreso.vue'
 import stockInsumos from '../views/stockInsumos.vue'
 import catalogoManu from '../views/catalogoManu.vue'
-import Stock from '../views/stockInsumos.vue'
+
 
 
 Vue.use(VueRouter)
@@ -33,11 +33,7 @@ Vue.use(VueRouter)
     component:catalogoManu
   },
 
-  {
-    path:'/stockInsumos',
-    name:'stock',
-    component:Stockdesarrollo
-  }
+ 
   
 ]
 
--- frontelbuensabor/src/views/catalogoManu.vue
+++ frontelbuensabor/src/views/catalogoManu.vue
@@ -1,14 +1,29 @@
 <template>
- <row>
   <div>
     <div class="header"></div>
     <div id="nav"><menuLateral></menuLateral>    
     </div>
 
     <div class="costado"></div>
-    <section class="informacion">
-      <h1>Catalogo manufacturados</h1>
-    </section>
+    <b-container class="informacion">
+      <h1>Catálogo manufacturados</h1>
+      <b-nav-form class="buscador">
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar manufacturado"></b-form-input>
+          <b-button size="sm" class="botonImagen" type="submit"><img src="@/assets/images/sistema/buscar.png" id="imagenBuscar"/></b-button>
+        </b-nav-form>
+        <b-dropdown right text="Filtrar por categoría" class="filtroCategoria" variant="white">
+        <b-dropdown-item>Pizza</b-dropdown-item>
+        <b-dropdown-item>Hamburguesa</b-dropdown-item>
+        <b-dropdown-item>Papas</b-dropdown-item>
+        <b-dropdown-item>Postre</b-dropdown-item>
+        </b-dropdown>
+        <b-table hover responsive  :items="manufacturadosData" :fields="tituloTabla"  :outlined=true :per-page="perPage" :current-page="currentPage" :borderless=true id="tablaManufac" class="tabla">
+            
+       </b-table>    
+       <b-button pill class="boton" size="md">Nuevo</b-button>
+       <b-pagination v-model="currentPage" size="sm" align="right" :total-rows="rows" :per-page="perPage"  aria-controls="my-tablaInsumos" class="paginador">
+        </b-pagination>
+    </b-container>
     
     <router-view />
   </div>
@@ -17,11 +32,45 @@
 <script>
 import MenuLateral from "@/components/MenuLateral.vue";
 export default {
+   mounted() {
+    this.getInsumos();
+  },
   components: {
     "menuLateral": MenuLateral
-  }
+  },
+  data() {
+      return {
+        perPage: 7,
+         currentPage: 1,        
+        tituloTabla: ['denominación','categoría', 'stock', 'tiempo'],
+        manufacturadosData:[],
+        manufacturados:{
+          denominacion:"",
+          categoria:"",
+          stock:"",
+          tiempo:"",
+        }
+      }
+    },
+    methods: {
+      async getInsumos(){
+         const res = await fetch("/manufacturados.json");
+        const resJson = await res.json();
+        this.manufacturadosData = resJson.manufacturados;
+        console.log(this.manufacturadosData);
+      },
+
+      agregarInsumo(){
+
+      }
+    },
+    computed: {
+      rows() {
+        return this.manufacturadosData.length
+      }
+    }
   
-};
+}
 </script>
 <style>
 h1{
@@ -29,7 +78,9 @@ h1{
   font-weight: 600;
   font-size: 18pt; 
   color:#151515;
+  text-align: left;
 }
+
 
 .costado{
     background-image: url("../assets/images/sistema/reticulaVertical.png");
@@ -42,19 +93,85 @@ h1{
 }
 
 .informacion{
-  background-color: red;
+  
   width: 50%;
   min-height: 100px; 
   margin-left: 30%;
   padding: 20px;
   padding-top:50px;
+  font-family: 'Baloo Bhaina 2';
+  color:#151515;
+
 }
 
 .header{
   background-color: blue;
   width: 100%;
   top:0;
-  min-height: 100px;  
+  min-height: 200px;  
+  
+}
+.buscador{
+  float:right;
+  margin-bottom: 25px;
+}
+
+
+.botonImagen{
+  background-color:transparent;
+  border:none;
+  padding: 0px;
+}
+.botonImagen:hover{
+  background-color:transparent;
+  border:none;
+}
+
+.botonImagen:active{
+  background-color:transparent;
+  border:none;
+}
+.botonImagen:visited{
+  background-color:transparent;
+  border:none;
+}
+
+.botonImagen:focus{
+  background-color:transparent;
+  border:none;
+}
+
+#imagenBuscar{
+  width: 25px;
+}
+
+#imagenAgregar{
+  width: 20px;
+  margin:0px;
+}
+
+.tabla{
+  text-align: left;
+}
+
+.page-link{
+   color: #151515;
+  border:none;
+  
+}
+.boton{
+  float:left;
+  background-color: #E7511E;
+  width: 105px;
+  height: 30px;
+  
+}
+
+.filtroCategoria{
+  float:left;
+  margin-bottom: 25px;
+  border:1px solid lightgray;
+  height: 32px;
   
 }
 
@@ -67,11 +184,30 @@ h1{
     }
 
     .informacion{
-      background-color: red;
-      width: 60%;
+      width: auto;
       min-height: 100px; 
-      margin-left: 20%;
+      margin-left: 60px;
+      
     }
+    .botonImagen{
+      display: inline;
+    }
+}
+
+
+@media screen and (max-width: 604px){
+    .informacion{
+      font-size: 10pt; 
+         
+    }
+    .table td, .table th{
+      padding:0.4rem;
+    }
+    .buscador{
+      float:left;
+     
+    }
+ 
 }
 
 </style>
