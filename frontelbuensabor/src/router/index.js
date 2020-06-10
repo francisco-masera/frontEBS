3850b1281719024b4782c1629b898f4818a55639
Georgina <georgi888111@gmail.com>
2020-06-10 15:49:25 -0300

    avances paso 1

--- frontelbuensabor/src/router/index.js
+++ frontelbuensabor/src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Ingreso from '../views/Ingreso.vue'
-import stockInsumos from '../views/stockInsumos.vue'
-import catalogoManu from '../views/catalogoManu.vue'
 import AñadirInsumo from '../views/AñadirInsumo.vue'
 
 Vue.use(VueRouter);
--- frontelbuensabor/src/views/AñadirInsumo.vue
+++ frontelbuensabor/src/views/AñadirInsumo.vue
@@ -9,7 +9,7 @@
     <b-container class="informacion">
       <h1>Añadir insumo</h1>
       <h2>Información básica</h2>
-      <b-form inline>
+      <b-form inline class="estiloForm"> 
         <table>
           <tr>
             <td>
@@ -119,41 +119,7 @@ export default {
 };
 </script>
 <style>
-h1 {
-  font-family: "Baloo Bhaina 2";
-  font-weight: 600;
-  font-size: 18pt;
-  color: #151515;
-  text-align: left;
-}
 
-h2 {
-  font-family: "Baloo Bhaina 2";
-  font-weight: 500;
-  font-size: 15pt;
-  color: #151515;
-  text-align: left;
-}
-
-.costado {
-  background-image: url("../assets/images/sistema/reticulaVertical.png");
-  background-repeat: repeat-y;
-  position: absolute;
-  left: 0;
-  height: 100%;
-  min-width: 200px;
-  background-size: contain;
-}
-
-.informacion {
-  width: 50%;
-  min-height: 100px;
-  margin-left: 30%;
-  padding: 20px;
-  padding-top: 50px;
-  font-family: "Baloo Bhaina 2";
-  color: #151515;
-}
 
 .header {
   background-color: blue;
@@ -161,93 +127,21 @@ h2 {
   top: 0;
   min-height: 200px;
 }
-.buscador {
-  float: right;
-  margin-bottom: 25px;
-}
 
-.botonImagen {
-  background-color: transparent;
-  border: none;
-  padding: 0px;
-}
-.botonImagen:hover {
-  background-color: transparent;
-  border: none;
-}
-
-.botonImagen:active {
-  background-color: transparent;
-  border: none;
-}
-.botonImagen:visited {
-  background-color: transparent;
-  border: none;
-}
-
-.botonImagen:focus {
-  background-color: transparent;
-  border: none;
-}
-
-#imagenBuscar {
-  width: 25px;
-}
-
-#imagenAgregar {
-  width: 20px;
-  margin: 0px;
-}
-
-.tabla {
+.estiloForm{
+  font-size: 13px;
   text-align: left;
 }
 
-.page-link {
-  color: #151515;
-  border: none;
-}
-.boton {
-  float: left;
-  background-color: #e7511e;
-  width: 105px;
-  height: 30px;
-}
 
-.filtroCategoria {
-  float: left;
-  margin-bottom: 25px;
-  border: 1px solid lightgray;
-  height: 32px;
-}
 
-@media screen and (max-width: 774px) {
-  .costado {
-    background-image: url("../assets/images/sistema/reticulaVertical-mobile.png");
-    width: 60px;
-    min-width: 60px;
-  }
 
-  .informacion {
-    width: auto;
-    min-height: 100px;
-    margin-left: 60px;
-  }
-  .botonImagen {
-    display: inline;
-  }
-}
 
-@media screen and (max-width: 604px) {
-  .informacion {
-    font-size: 10pt;
-  }
-  .table td,
-  .table th {
-    padding: 0.4rem;
-  }
-  .buscador {
-    float: left;
-  }
-}
+
+
+
+
+
+
+
 </style>
