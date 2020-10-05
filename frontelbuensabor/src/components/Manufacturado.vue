5f8625972a24d4327c597242870fbfab9535290e
Dargor94 <franciscomasera1994@gmail.com>
2020-10-05 20:21:20 -0300

    imagenes home

--- frontelbuensabor/src/components/Manufacturado.vue
+++ frontelbuensabor/src/components/Manufacturado.vue
@@ -1,16 +1,24 @@
 <template>
   <b-card class="tarjeta">
-      <b-card-img class="imagen" :src="'http://localhost:9001/images/productos/' + manufacturadoParam.imagen"/>
-      <strong>{{manufacturadoParam.denominacion}}</strong>
+    <b-container>
+      <b-card-img
+        style="height: auto; max-height: 180px"
+        class="imagen img-fluid"
+        :src="
+          'http://localhost:9001/images/productos/' + manufacturadoParam.imagen
+        "
+      />
+      <strong>{{ manufacturadoParam.denominacion }}</strong>
+    </b-container>
+    <b-container style="text-align: left">
       <b-card-text>
-       {{ manufacturadoParam.descripcion }}
+        {{ manufacturadoParam.descripcion }}
       </b-card-text>
-      <b-card-text class="precio">
-       ${{ manufacturadoParam.precioVenta}}
+      <b-card-text class="precio" style="float:right;">
+        ${{ manufacturadoParam.precioVenta }}
       </b-card-text>
-      
-    </b-card>
-  
+    </b-container>
+  </b-card>
 </template>
 
 <script>
@@ -20,13 +28,13 @@ export default {
 </script>
 
 <style>
-.tarjeta{
-    background-color: transparent;
-    border:none;
+.tarjeta {
+  background-color: transparent;
+  border: none;
 }
-.precio{
-    font-size: 16pt;
-    font-weight: 600;
-    text-align: right;
+.precio {
+  font-size: 16pt;
+  font-weight: 600;
+  text-align: right;
 }
 </style>
--- frontelbuensabor/src/views/Home.vue
+++ frontelbuensabor/src/views/Home.vue
@@ -1,51 +1,66 @@
 <template>
   <div id="home">
-    <div id=home1>
+    <div id="home1">
       <cabecera v-bind:es-home="true"></cabecera>
       <div id="buscador">
         <h1>Lo pedís, lo llevamos...</h1>
-        <b-form-input id="inputBuscador"></b-form-input>  
-        <b-button
-            size="sm"
-            class="botonImagen"
-          >
-            <img src="http://localhost:9001/images/sistema/buscar.png" id="imagenBuscar" />
-          </b-button>      
+        <b-form-input id="inputBuscador"></b-form-input>
+        <b-button size="sm" class="botonImagen">
+          <img
+            src="http://localhost:9001/images/sistema/buscar.png"
+            id="imagenBuscar"
+          />
+        </b-button>
       </div>
       <div id="pasosCuadro">
         <div class="pasos">
-          <img src="http://localhost:9001/images/sistema/icono pasos-01.png" width="70px" class="imgPasos">
+          <img
+            src="http://localhost:9001/images/sistema/icono pasos-01.png"
+            width="70px"
+            class="imgPasos"
+          />
           <div class="pasosTexto">
-             <strong>Seleccioná</strong> los productos<br> que quieras.
-          </div>         
+            <strong>Seleccioná</strong> los productos<br />
+            que quieras.
+          </div>
         </div>
         <div class="pasos">
-          <img src="http://localhost:9001/images/sistema/icono pasos-02.png" width="90px" class="imgPasos">
+          <img
+            src="http://localhost:9001/images/sistema/icono pasos-02.png"
+            width="90px"
+            class="imgPasos"
+          />
           <div class="pasosTexto">
-             <strong>Elegí</strong> el método de<br> pago que prefieras.
-          </div>         
+            <strong>Elegí</strong> el método de<br />
+            pago que prefieras.
+          </div>
         </div>
         <div class="pasos">
-          <img src="http://localhost:9001/images/sistema/icono pasos-03.png" width="90px" class="imgPasos">
+          <img
+            src="http://localhost:9001/images/sistema/icono pasos-03.png"
+            width="90px"
+            class="imgPasos"
+          />
           <div class="pasosTexto">
-             <strong>Seleccioná</strong> la forma<br> de entrega.
-          </div>         
+            <strong>Seleccioná</strong> la forma<br />
+            de entrega.
+          </div>
         </div>
-        
-      
       </div>
-   
-  </div>
-  <div id="home2">
-    <h2>Los que todos amamos</h2>
+    </div>
+    <div id="home2">
+      <h2>Los que todos amamos</h2>
 
       <b-card-group deck>
-        <div class="contenedorTarjeta"
+        <div
+          class="contenedorTarjeta"
           v-for="manufacturado in manufacturadosData"
           :key="manufacturado.id"
-         
         >
-          <plato-item :manufacturadoParam="manufacturado" class="tarjetaPlato"></plato-item>
+          <plato-item
+            :manufacturadoParam="manufacturado"
+            class="tarjetaPlato"
+          ></plato-item>
         </div>
       </b-card-group>
     </div>
@@ -55,129 +70,117 @@
   </div>
 </template>
 <script>
-
 import Header from "@/components/Header.vue";
 import Plato from "@/components/Manufacturado.vue";
 import Service from "@/service/Service.js";
 
 export default {
- 
-  components: {    
+  components: {
     cabecera: Header,
     "plato-item": Plato,
   },
-  mounted() {    
+  mounted() {
     this.getManufacturados();
-    
   },
   data() {
     return {
-     
       manufacturadosData: [],
       service: new Service(),
-   
     };
   },
-  methods: {    
-  
+  methods: {
     async getManufacturados() {
-      await this.service.getAll("manufacturado").then(data => {
-        this.manufacturadosData = data;        
-       
+      await this.service.getAll("manufacturado").then((data) => {
+        this.manufacturadosData = data;
       });
     },
-}
-}
+  },
+};
 </script>
 <style>
-#buscador h1{
-  color:#FFFFFF;
+#buscador h1 {
+  color: #ffffff;
   font-family: "Baloo Bhaina 2";
   font-weight: 600;
   font-size: 22pt;
 }
 
-h2{
-   color: #151515;
+h2 {
+  color: #151515;
   font-family: "Baloo Bhaina 2";
   font-weight: 600;
   font-size: 22pt;
 }
-#home{
-  background-color: #F2E6D1;
- 
+#home {
+  background-color: #f2e6d1;
 }
-#home1{  
+#home1 {
   background-image: url("http://localhost:9001/images/sistema/fondo.png");
   background-repeat: no-repeat;
   background-position: center;
-  background-size:cover;
+  background-size: cover;
   min-height: 750px;
-  width: 100%;  
+  width: 100%;
 }
 
-#home2{
+#home2 {
   min-height: 50px;
-   padding-left: 150px;
+  padding-left: 150px;
   padding-right: 150px;
   padding-top: 80px;
 }
-#inputBuscador{
+#inputBuscador {
   background-color: rgba(255, 255, 255, 0.5);
   border-color: black;
-  border:2;
+  border: 2;
   width: 90%;
 }
-#buscador{
+#buscador {
   width: 40%;
-  margin-top:40px;
+  margin-top: 40px;
   margin-left: auto;
   margin-right: auto;
 }
-#pasosCuadro{
+#pasosCuadro {
   margin-top: 220px;
   min-height: 50px;
   margin-left: 100px;
-   margin-right: 100px;
+  margin-right: 100px;
 }
-.pasos{
-  color:white;
+.pasos {
+  color: white;
   font-family: "Baloo Bhaina 2";
   font-weight: 400;
   font-size: 14pt;
-  float:left;
+  float: left;
   margin-left: 40px;
   margin-right: 40px;
 }
-.pasosTexto{
+.pasosTexto {
   display: block;
-  float:left;
+  float: left;
   padding: 20px;
 }
-.imgPasos{
-  float:left;
+.imgPasos {
+  float: left;
 }
 
-.botonImagen{
-float:left;
+.botonImagen {
+  float: left;
 }
 
-.tarjetaPlato{
+.tarjetaPlato {
   width: 100%;
-  float:left;
-  
+  float: left;
 }
-.contenedorTarjeta{
-  margin:20px;
+.contenedorTarjeta {
+  margin: 20px;
   width: 25%;
   background-color: transparent;
+  display: grid;
 }
-.imagenBuscar{
-float:left;
-width: 10px;
+.imagenBuscar {
+  float: left;
+  width: 10px;
 }
 </style>
-
-
-
-
