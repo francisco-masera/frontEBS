05ff2aeeb0d09d3aabf8b981cca2a182282b9534
rooespinoza <rocio.espinoza@alumnos.frm.utn.edu.ar>
2021-05-30 19:49:11 -0300

    Filtra en mas pedidos

--- frontelbuensabor/src/views/Home.vue
+++ frontelbuensabor/src/views/Home.vue
@@ -4,11 +4,15 @@
       <cabecera v-bind:es-home="true"></cabecera>
       <div id="buscador">
         <h1>Lo pedís, lo llevamos...</h1>
-        <b-form-input id="inputBuscador" placeholder="¿Qué comemos hoy?" v-model="buscarText"> </b-form-input>
-        <b-button size="sm" class="botonImagenBuscador"  @click="buscar()">
-          <img
-            src="http://localhost:9001/images/sistema/buscar.png"
-          />
+        <b-form-input
+          id="inputBuscador"
+          placeholder="¿Qué comemos hoy?"
+          v-model="buscar"
+          @keyup="filtrar"
+        >
+        </b-form-input>
+        <b-button size="sm" class="botonImagenBuscador">
+          <img src="http://localhost:9001/images/sistema/buscar.png" />
         </b-button>
       </div>
       <div v-if="windowMobile" id="pasosCuadro--mobile">
@@ -46,7 +50,7 @@
           </div>
         </div>
       </div>
-      <div v-else  id="pasosCuadro">
+      <div v-else id="pasosCuadro">
         <div class="pasos">
           <img
             src="http://localhost:9001/images/sistema/icono pasos-01.png"
@@ -83,7 +87,6 @@
       </div>
     </div>
     <div id="home2">
-
       <b-card-group class="platos">
         <h2>Los que todos amamos</h2>
         <div v-if="windowMobile" class="contenedorCard--mobile">
@@ -92,10 +95,8 @@
             v-for="manufacturado in venta"
             :key="manufacturado.id"
           >
-            <plato-item
-              :manufacturadoParam="manufacturado"
-            ></plato-item>
-        </div>
+            <plato-item :manufacturadoParam="manufacturado"></plato-item>
+          </div>
         </div>
         <div v-else class="contenedorCard">
           <div
@@ -103,69 +104,73 @@
             v-for="manufacturado in venta"
             :key="manufacturado.id"
           >
-            <plato-item
-              :manufacturadoParam="manufacturado"
-            ></plato-item>
+            <plato-item :manufacturadoParam="manufacturado"></plato-item>
+          </div>
         </div>
-        </div>
-        
-      
       </b-card-group>
-      <br>
-      <b-button class="botonPlatos" @click="$router.push({ path:'/menu'})">¡Todos nuestros platos!</b-button>
+      <br />
+      <b-button class="botonPlatos" @click="$router.push({ path: '/menu' })"
+        >¡Todos nuestros platos!</b-button
+      >
     </div>
 
-      <div class="abajo"></div>
-      <div id="home3">
-        <div class="cuadro">
-          <img
-            src="http://localhost:9001/images/sistema/nosotros.png"
-          />
+    <div class="abajo"></div>
+    <div id="home3">
+      <div class="cuadro">
+        <img src="http://localhost:9001/images/sistema/nosotros.png" />
+      </div>
+      <div class="cuadro" style="background-color: #1f91b6">
+        <div class="cuadroText">
+          <h1 style="color: white">Un poco sobre nosotros</h1>
+          Comenzamos este emprendimiento en el año 2017, con la esperanza de
+          poder crecer como profesionales y generar una gran amistad entre
+          nosotros. Nos enfocamos en un producto de calidad, intentando brindar
+          lo mejor de nosotros para que nuestros clientes queden satisfechos.
+          ¡Esperamos verte nuevamente por aquí!
         </div>
-        <div class="cuadro" style="background-color:#1f91b6">
-            <div class="cuadroText">
-              <h1 style="color:white">Un poco sobre nosotros</h1>
-              Comenzamos este emprendimiento en el año 2017, con la esperanza de poder crecer como profesionales y generar una gran amistad entre nosotros. Nos enfocamos en un producto de calidad, intentando brindar lo mejor de nosotros para que nuestros clientes queden satisfechos. 
-              ¡Esperamos verte nuevamente por aquí!
-            </div>
-        </div>
-        
-        <div class="cuadro" style="background-color:#e7511e">
-            <div class="cuadroText">
-              <h1 style="color:white">¡Contáctanos!</h1>
-              <div class="enlaces">
-              <a :href="'//www.facebook.com/elbuensabor'" target="_blank"><img src='images/facebook.svg'>ElBuenSabor</a>
-              <br/>
-              <a :href="'//www.instagram.com/elbuensabor'" target="_blank"><img src='images/instagram.svg'>elbuensabor</a>
-               <br/>
-              <a :href="'mailto:info@elbuensabor.com'" target="_blank"><img src='images/correo.svg'>info@elbuensabor.com</a>
-               <br/>
-              <a :href="'tel:+54 261 156789873'" ><img src='images/telefono.svg'>+54 261 156789873</a>
-              <br/>
-              <a :href="'https://maps.google.com/?q=-32.890848,-68.839388'" ><img src='images/direccion.svg'>San Martín 1087, Mendoza</a>
-            </div>
-            </div>
-        </div>
-        <div class="cuadro">
-          <iframe
-            :src="
-              'https://maps.google.com/maps?q=-32.890848,-68.839388&z=15&output=embed'
-            "
-            width="100%"
-            height="100%"
-            frameborder="0"
-            style="border: 0"
-            ></iframe>
-          </div>
+      </div>
 
+      <div class="cuadro" style="background-color: #e7511e">
+        <div class="cuadroText">
+          <h1 style="color: white">¡Contáctanos!</h1>
+          <div class="enlaces">
+            <a :href="'//www.facebook.com/elbuensabor'" target="_blank"
+              ><img src="images/facebook.svg" />ElBuenSabor</a
+            >
+            <br />
+            <a :href="'//www.instagram.com/elbuensabor'" target="_blank"
+              ><img src="images/instagram.svg" />elbuensabor</a
+            >
+            <br />
+            <a :href="'mailto:info@elbuensabor.com'" target="_blank"
+              ><img src="images/correo.svg" />info@elbuensabor.com</a
+            >
+            <br />
+            <a :href="'tel:+54 261 156789873'"
+              ><img src="images/telefono.svg" />+54 261 156789873</a
+            >
+            <br />
+            <a :href="'https://maps.google.com/?q=-32.890848,-68.839388'"
+              ><img src="images/direccion.svg" />San Martín 1087, Mendoza</a
+            >
+          </div>
+        </div>
       </div>
-      <div class="footer">
-        <img
-            src="http://localhost:9001/images/sistema/logo-01.png"
-          />
+      <div class="cuadro">
+        <iframe
+          :src="'https://maps.google.com/maps?q=-32.890848,-68.839388&z=15&output=embed'"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          style="border: 0"
+        ></iframe>
       </div>
-      <div class="abajo"></div>
-      <Loader v-if="loading" :loading="loading" />
+    </div>
+    <div class="footer">
+      <img src="http://localhost:9001/images/sistema/logo-01.png" />
+    </div>
+    <div class="abajo"></div>
+    <Loader v-if="loading" :loading="loading" />
   </div>
 </template>
 <script>
@@ -174,57 +179,69 @@ import Plato from "@/components/Manufacturado.vue";
 import Service from "@/service/Service.js";
 import Loader from "@/components/Loader.vue";
 import Utils from "@/utilidades/Utils.js";
-export default new Router({
-    routes: [
-        {
-            path: '/Menu/:id',
-            name: 'Menu',
-            component: this.buscarText,
-        },
-    ]
-});
 export default {
   components: {
     cabecera: Header,
     "plato-item": Plato,
-    Loader: Loader
+    Loader: Loader,
   },
   mounted() {
     this.getAllProductos();
-      if(window.innerWidth<=426){
-        this.windowMobile = true
-      }else{
-        this.windowMobile = false
-      }
-     
+    if (window.innerWidth <= 426) {
+      this.windowMobile = true;
+    } else {
+      this.windowMobile = false;
+    }
   },
   data() {
     return {
-      windowHeight:false,
+      windowHeight: false,
       venta: [],
       service: new Service(),
       loading: true,
       utils: new Utils(),
-      buscarText:""
+      buscar: "",
+      allProductos: [],
+      filtrados: [],
+      timeout: null,
     };
   },
   methods: {
-      getAllProductos() {
+    getAllProductos() {
       this.service
         .getAll("manufacturado/masVendidos")
-        .then((r) => r.forEach((d) => this.venta.push(d)))
+        .then((r) => r.forEach((d) => this.allProductos.push(d)))
         .then(
-          this.service
-            .getAll("insumoVenta/masVendidos")
-            .then((r) => {
-              r.forEach((d) => this.venta.push(d));
-              this.loading= false;
-            })
-        ).catch(this.loading= false)
+          this.service.getAll("insumoVenta/masVendidos").then((r) => {
+            r.forEach((d) => this.allProductos.push(d));
+            this.loading = false;
+          })
+        )
+        .catch((this.loading = false));
+        this.venta=this.allProductos
     },
-    buscar(){
-
-    }
+    filtrar() {
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        this.filtrados = [];
+        if (this.buscar != "") {
+          this.filtrados = this.allProductos.filter((item) => {
+            if (item.type == "ArticuloManufacturado") {
+              return item.denominacion
+                .toLowerCase()
+                .includes(this.buscar.toLowerCase());
+            } else {
+              return item.insumo.denominacion
+                .toLowerCase()
+                .includes(this.buscar.toLowerCase());
+            }
+          });
+          this.venta=this.filtrados;
+        } else{
+          this.venta=this.allProductos
+        }
+      }, 800);
+    },
   },
 };
 </script>
@@ -264,9 +281,9 @@ h2 {
   border-color: black;
   border: 2;
   width: 100%;
-  color:black;
+  color: black;
 }
-#inputBuscador::placeholder{
+#inputBuscador::placeholder {
   color: white;
 }
 #buscador {
@@ -280,12 +297,12 @@ h2 {
   min-height: 50px;
   text-align: center;
   width: 100%;
-  display:flex;
+  display: flex;
   justify-content: center;
   margin-left: 5px;
   margin-right: 5px;
 }
-#pasosCuadro--mobile{
+#pasosCuadro--mobile {
   display: none;
 }
 .pasos {
@@ -313,13 +330,13 @@ h2 {
   padding: 0px;
   outline: none;
   position: relative;
-    float: right;
-    z-index: 98;
-    min-width: 0px;
-    margin-top: -30px;
-    margin-right: 10px;
+  float: right;
+  z-index: 98;
+  min-width: 0px;
+  margin-top: -30px;
+  margin-right: 10px;
 }
-.botonImagenBuscador img{
+.botonImagenBuscador img {
   width: 60%;
 }
 
@@ -345,12 +362,12 @@ h2 {
   border: none !important;
   outline: none;
 }
-.platos{
+.platos {
   width: 70%;
   display: inline-block;
   text-align: center;
 }
-.contenedorCard{
+.contenedorCard {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -363,21 +380,21 @@ h2 {
   min-width: 200px;
   margin-bottom: 50px;
 }
-.contenedorCard--mobile{
+.contenedorCard--mobile {
   display: none;
 }
-.card-body{
-  padding:0px;
+.card-body {
+  padding: 0px;
   text-align: left;
 }
 
-.botonPlatos{
+.botonPlatos {
   background-color: #e7511e;
   color: #ffffff;
   font-weight: 600;
   margin-bottom: 90px;
 }
-.botonPlatos:hover{
+.botonPlatos:hover {
   border: #e7511e;
   background-color: #ffffff;
   border-style: solid;
@@ -385,137 +402,136 @@ h2 {
   color: #e7511e;
 }
 
-.botonPlatos:visited{
-   background-color: #e7511e;
+.botonPlatos:visited {
+  background-color: #e7511e;
   color: #ffffff;
 }
-.botonPlatos:focus{
-   background-color: #e7511e;
+.botonPlatos:focus {
+  background-color: #e7511e;
   color: #ffffff;
 }
-.botonPlatos:visited{
-   background-color: #e7511e;
+.botonPlatos:visited {
+  background-color: #e7511e;
   color: #ffffff;
 }
 
-.botonPlatos:active{
-   border: #e7511e!important;
+.botonPlatos:active {
+  border: #e7511e !important;
   background-color: #ffffff !important;
-  border-style: solid!important;
-  border-width: 1px!important;
-  color: #e7511e!important;
+  border-style: solid !important;
+  border-width: 1px !important;
+  color: #e7511e !important;
 }
 
-#home3{
+#home3 {
   width: 100%;
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;  
+  flex-wrap: wrap;
 }
-.cuadro{
+.cuadro {
   width: 50%;
   display: inline-block;
   height: 428px;
 }
-.enlaces{
+.enlaces {
   padding-left: 20px;
 }
-.enlaces a{
+.enlaces a {
   font-family: "Baloo Bhaina 2";
-    color: white;
-    font-size: 16pt;
-  }
+  color: white;
+  font-size: 16pt;
+}
 
-.cuadro img{
+.cuadro img {
   width: 100%;
   height: auto;
 }
-.enlaces a img{
+.enlaces a img {
   display: inline-block;
   margin-top: -4px;
   margin-right: 5px;
-  width:22px;
+  width: 22px;
 }
-.cuadroText{
+.cuadroText {
   width: 80%;
   margin: 10%;
   text-align: left;
   color: white;
 }
 
-.footer{
+.footer {
   background-color: #1f91b6;
   text-align: center;
   width: 100%;
   padding: 30px;
 }
-.footer img{
+.footer img {
   width: 20%;
 }
-@supports(object-fit: cover){
-    .cuadro img{
-      height: 100%;
-      object-fit: cover;
-      object-position: center center;
-    }
+@supports (object-fit: cover) {
+  .cuadro img {
+    height: 100%;
+    object-fit: cover;
+    object-position: center center;
+  }
 }
-@media (max-width: 1600px) { 
-    .platos{
-      width: 80%;
-    }
+@media (max-width: 1600px) {
+  .platos {
+    width: 80%;
+  }
 }
 @media (max-width: 1280px) {
-  .platos{
-  width: 90%;
-}
+  .platos {
+    width: 90%;
+  }
 }
 @media (max-width: 769px) {
   .contenedorTarjeta {
     width: 300px;
     min-width: inherit;
   }
- .cuadro{
-   width: 100%;
- }
- .enlaces a{
+  .cuadro {
+    width: 100%;
+  }
+  .enlaces a {
     font-size: 14pt;
   }
-  }
-  
+}
+
 @media (max-width: 640px) {
-   #buscador {
+  #buscador {
     width: 70%;
   }
   #pasosCuadro {
-  margin-top: 150px;
-}
- .contenedorTarjeta {
+    margin-top: 150px;
+  }
+  .contenedorTarjeta {
     width: 250px;
     min-width: inherit;
   }
-
 }
 
 @media (max-width: 426px) {
   #pasosCuadro--mobile {
     margin-top: 220px;
     min-height: 50px;
-      height: 100%;
+    height: 100%;
     width: 100vw;
     display: flex;
     overflow-x: scroll;
-  text-align: left;
-  margin-left: 5%;
-  margin-right: 5%;
+    text-align: left;
+    margin-left: 5%;
+    margin-right: 5%;
   }
-  .pasos{
+  .pasos {
     min-width: 340px;
     margin: 10px;
   }
   #buscador {
     width: 80%;
   }
-  .contenedorCard--mobile{
+  .contenedorCard--mobile {
     height: 100%;
     width: 100vw;
     display: flex;
@@ -525,29 +541,27 @@ h2 {
     margin-right: 30px;
     padding-right: 40px;
   }
-  .contenedorTarjeta{
+  .contenedorTarjeta {
     min-width: 300px;
   }
-  .footer img{
-  width: 40%;
-}
+  .footer img {
+    width: 40%;
+  }
 }
 @media (max-width: 375px) {
- #buscador h1{
+  #buscador h1 {
     font-size: 20pt;
   }
 }
 @media (max-width: 320px) {
-   
-  #buscador h1{
+  #buscador h1 {
     font-size: 18pt;
   }
-.enlaces{
-  padding-left: 10px;
+  .enlaces {
+    padding-left: 10px;
+  }
+  .footer img {
+    width: 60%;
+  }
 }
- .footer img{
-  width: 60%;
-}
-}
-
 </style>
